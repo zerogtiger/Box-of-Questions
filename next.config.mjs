9f1215208d5d39145a944ac24eb1e7b00171d5c0
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2024-06-16 10:51:01 -0400

    Removed unnecessary pages, set redirects from root path to login

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig;
